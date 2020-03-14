93ad8fbf6d0022080d1b145f8de376c47234e0ce
a.kashin <al.s.kashin@gmail.com>
2020-03-14 17:59:34 +0500

    завершение процесса

--- modules/web/src/ru/kamotive/kd/web/screens/kdexchange/KdExchangeEdit.java
+++ modules/web/src/ru/kamotive/kd/web/screens/kdexchange/KdExchangeEdit.java
@@ -1,11 +1,26 @@
 package ru.kamotive.kd.web.screens.kdexchange;
 
+import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.screen.*;
+import ru.kamotive.kd.core.ExecuteTaskService;
+import ru.kamotive.kd.entity.ExchangeStatus;
 import ru.kamotive.kd.entity.KdExchange;
+
+import javax.inject.Inject;
 
 @UiController("kd_KdExchange.edit")
 @UiDescriptor("kd-exchange-edit.xml")
 @EditedEntityContainer("kdExchangeDc")
 @LoadDataBeforeShow
 public class KdExchangeEdit extends StandardEditor<KdExchange> {
+    @Inject
+    private ExecuteTaskService executeTaskService;
+
+    @Subscribe("confirmBtn")
+    public void onConfirmBtnClick(Button.ClickEvent event) {
+        executeTaskService.executeTask(getEditedEntity());
+        getEditedEntity().setState(ExchangeStatus.DONE);
+        commitChanges();
+    }
+
 }
