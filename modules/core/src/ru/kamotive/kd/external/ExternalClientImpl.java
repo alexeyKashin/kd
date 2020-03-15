daf35531118c7620eefd86e493c278066b753bf8
a.kashin <al.s.kashin@gmail.com>
2020-03-15 10:17:51 +0500

    расширение темы, иконки

---
+++ modules/core/src/ru/kamotive/kd/external/ExternalClientImpl.java
@@ -0,0 +1,16 @@
+package ru.kamotive.kd.external;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExternalClientImpl implements ExternalClient {
+    @Override
+    public String executeStart() {
+        return null;
+    }
+
+    @Override
+    public String getLinks() {
+        return null;
+    }
+}
