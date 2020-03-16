6348c1dfaf1e484be89762dd3237340ce54656a8
unmatty <unesihin.m.v@gmail.com>
2020-03-17 00:53:13 +0500

    десктоп + поведение кнопки

--- modules/web/src/ru/kamotive/kd/web/screens/config/AccessPlatfomConfig.java
+++ modules/web/src/ru/kamotive/kd/web/screens/config/AccessPlatfomConfig.java
@@ -1,10 +1,23 @@
 package ru.kamotive.kd.web.screens.config;
 
+import com.haulmont.cuba.gui.Notifications;
+import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.Subscribe;
 import com.haulmont.cuba.gui.screen.UiController;
 import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+import javax.inject.Inject;
 
 @UiController("kd_AccessPlatfomConfig")
 @UiDescriptor("access-platfom-config.xml")
 public class AccessPlatfomConfig extends Screen {
+    @Inject
+    private Notifications notifications;
+
+    @Subscribe("leftCallBtn")
+    public void onLeftCallBtnClick(Button.ClickEvent event) {
+        notifications.create().withCaption("Заявка заведена!").show();
+        closeWithDefaultAction();
+    }
 }
---
+++ modules/web/src/ru/kamotive/kd/web/screens/custom/DesktopIngScreen.java
@@ -0,0 +1,10 @@
+package ru.kamotive.kd.web.screens.custom;
+
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("kd_DesktopIngScreen")
+@UiDescriptor("desktop-ing-screen.xml")
+public class DesktopIngScreen extends Screen {
+}
