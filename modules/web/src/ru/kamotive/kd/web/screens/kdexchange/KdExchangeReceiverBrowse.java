b89de8700a91d69e66a326e28846d73f444374e4
unmatty <unesihin.m.v@gmail.com>
2020-03-19 10:15:34 +0500

    порты

--- modules/web/src/ru/kamotive/kd/web/screens/kdexchange/KdExchangeReceiverBrowse.java
+++ modules/web/src/ru/kamotive/kd/web/screens/kdexchange/KdExchangeReceiverBrowse.java
@@ -1,9 +1,15 @@
 package ru.kamotive.kd.web.screens.kdexchange;
 
+import com.haulmont.cuba.gui.Notifications;
+import com.haulmont.cuba.gui.ScreenBuilders;
+import com.haulmont.cuba.gui.components.Button;
+import com.haulmont.cuba.gui.components.GroupTable;
 import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.cuba.security.global.UserSession;
+import ru.kamotive.kd.entity.ExchangeStatus;
 import ru.kamotive.kd.entity.KdExchange;
+import ru.kamotive.kd.web.screens.viewfile.ViewFileBrowse;
 
 import javax.inject.Inject;
 
@@ -16,10 +22,36 @@ public class KdExchangeReceiverBrowse extends StandardLookup<KdExchange> {
     private UserSession userSession;
     @Inject
     private CollectionLoader<KdExchange> kdExchangesDl;
+    @Inject
+    private ScreenBuilders screenBuilders;
+    @Inject
+    private GroupTable<KdExchange> kdExchangesTable;
+    @Inject
+    private Notifications notifications;
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
         kdExchangesDl.setParameter("currentUserId", userSession.getCurrentOrSubstitutedUser().getId());
         kdExchangesDl.load();
     }
+
+    @Subscribe("showDataTbl")
+    public void onShowDataTblClick(Button.ClickEvent event) {
+        KdExchange kdExchange = kdExchangesTable.getSingleSelected();
+
+        if (kdExchange.getState().equals(ExchangeStatus.DONE) || kdExchange.getState().equals(ExchangeStatus.DELIVERED)){
+            ViewFileBrowse viewFileBrowse = screenBuilders.screen(this)
+                    .withScreenClass(ViewFileBrowse.class)
+                    .build();
+
+            viewFileBrowse.setProcessName("Передача КД");
+            viewFileBrowse.setFileString(kdExchange.getDocumentLinks());
+            viewFileBrowse.setFileString("c:/temp/kamotivFiles/Kronshtein_Geom.html,c:/temp/kamotivFiles/model1.prt,c:/temp/kamotivFiles/images_preview.qaf,c:/temp/kamotivFiles/qafmetadata.qaf,c:/temp/kamotivFiles/MODEL$.jt");
+            viewFileBrowse.show();
+        } else {
+            notifications.create().withCaption("Доставка данных еще не выполнена!").show();
+        }
+    }
+    
+    
 }
--- modules/web/src/ru/kamotive/kd/web/screens/viewfile/ViewFileBrowse.java
+++ modules/web/src/ru/kamotive/kd/web/screens/viewfile/ViewFileBrowse.java
@@ -2,15 +2,15 @@ package ru.kamotive.kd.web.screens.viewfile;
 
 import com.haulmont.cuba.core.global.Metadata;
 import com.haulmont.cuba.gui.Notifications;
-import com.haulmont.cuba.gui.components.BrowserFrame;
-import com.haulmont.cuba.gui.components.ClasspathResource;
-import com.haulmont.cuba.gui.components.Image;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
+import com.haulmont.cuba.gui.screen.LookupComponent;
 import ru.kamotive.kd.entity.ViewFile;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 @UiController("kd_ViewFile.browse")
 @UiDescriptor("view-file-browse.xml")
@@ -27,55 +27,131 @@ public class ViewFileBrowse extends StandardLookup<ViewFile> {
     private Notifications notifications;
     @Inject
     private BrowserFrame browseFrame;
+    @Inject
+    private Label<String> kdNameLbl;
+    @Named("tabSheet.dopFileView")
+    private VBoxLayout dopFileView;
+    @Inject
+    private CollectionContainer<ViewFile> viewFilesDc2;
+
+    private String processName;
+    private String fileString;
+
+    public void setFileString(String fileString) {
+        this.fileString = fileString;
+    }
+
+    public void setProcessName(String processName) {
+        this.processName = processName;
+    }
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
-        // Corpus.prt
-        // Planka.dvg
-        // Lopatka.sim
+        if (processName == null || processName.isEmpty()) {
+            kdNameLbl.setVisible(false);
+            // Corpus.prt
+            // Planka.dvg
+            // Lopatka.sim
 
-        // ru/kamotive/kd/web/screens/img/
+            // ru/kamotive/kd/web/screens/img/
 
-        ViewFile lopatkaFile = metadata.create(ViewFile.class);
-        lopatkaFile.setName("Lopatka.sim");
-        lopatkaFile.setLink("ru/kamotive/kd/web/screens/viewfile/3.jpg");
-        viewFilesDc.getMutableItems().add(lopatkaFile);
+            ViewFile lopatkaFile = metadata.create(ViewFile.class);
+            lopatkaFile.setName("Lopatka.sim");
+            lopatkaFile.setLink("ru/kamotive/kd/web/screens/viewfile/3.jpg");
+            viewFilesDc.getMutableItems().add(lopatkaFile);
 
-        ViewFile plankaFile = metadata.create(ViewFile.class);
-        plankaFile.setName("Planka.dvg");
-        plankaFile.setLink("ru/kamotive/kd/web/screens/viewfile/2.jpg");
-        viewFilesDc.getMutableItems().add(plankaFile);
+            ViewFile plankaFile = metadata.create(ViewFile.class);
+            plankaFile.setName("Planka.dvg");
+            plankaFile.setLink("ru/kamotive/kd/web/screens/viewfile/2.jpg");
+            viewFilesDc.getMutableItems().add(plankaFile);
 
 
-        ViewFile corpusFile = metadata.create(ViewFile.class);
-        corpusFile.setName("Corpus.prt");
-        corpusFile.setLink("ru/kamotive/kd/web/screens/viewfile/1.jpg");
-        viewFilesDc.getMutableItems().add(corpusFile);
+            ViewFile corpusFile = metadata.create(ViewFile.class);
+            corpusFile.setName("Corpus.prt");
+            corpusFile.setLink("ru/kamotive/kd/web/screens/viewfile/1.jpg");
+            viewFilesDc.getMutableItems().add(corpusFile);
 
-        ViewFile kronshtain = metadata.create(ViewFile.class);
-        kronshtain.setName("Kronshtein.3D");
-        kronshtain.setLink("ru/kamotive/kd/web/screens/viewfile/Kronshtein.html");
-        viewFilesDc.getMutableItems().add(kronshtain);
+            ViewFile kronshtain = metadata.create(ViewFile.class);
+            kronshtain.setName("Kronshtein.3D");
+            kronshtain.setLink("ru/kamotive/kd/web/screens/viewfile/Kronshtein.html");
+            viewFilesDc.getMutableItems().add(kronshtain);
 
-        ViewFile kronshtainGeom = metadata.create(ViewFile.class);
-        kronshtainGeom.setName("Kronshtein_geom.3D");
-        kronshtainGeom.setLink("ru/kamotive/kd/web/screens/viewfile/Kronshtein_Geom.html");
-        viewFilesDc.getMutableItems().add(kronshtainGeom);
+            ViewFile kronshtainGeom = metadata.create(ViewFile.class);
+            kronshtainGeom.setName("Kronshtein_geom.3D");
+            kronshtainGeom.setLink("ru/kamotive/kd/web/screens/viewfile/Kronshtein_Geom.html");
+            viewFilesDc.getMutableItems().add(kronshtainGeom);
 
-        imageField.setVisible(false);
-        browseFrame.setVisible(false);
+            imageField.setVisible(false);
+            browseFrame.setVisible(false);
+        } else {
+            kdNameLbl.setVisible(true);
+            kdNameLbl.setValue(processName);
+
+            ViewFile lopatkaFile = metadata.create(ViewFile.class);
+            lopatkaFile.setName("images_preview.qaf");
+            viewFilesDc.getMutableItems().add(lopatkaFile);
+
+            ViewFile plankaFile = metadata.create(ViewFile.class);
+            plankaFile.setName("MODEL$.jt");
+            viewFilesDc.getMutableItems().add(plankaFile);
+
+
+            ViewFile corpusFile = metadata.create(ViewFile.class);
+            corpusFile.setName("model1.prt");
+            viewFilesDc.getMutableItems().add(corpusFile);
+
+            ViewFile kronshtain = metadata.create(ViewFile.class);
+            kronshtain.setName("qafmetdata.qaf");
+            viewFilesDc.getMutableItems().add(kronshtain);
+
+            ViewFile kronshtainGeom = metadata.create(ViewFile.class);
+            kronshtainGeom.setName("Kronshtein_geom.3D");
+            kronshtainGeom.setLink("ru/kamotive/kd/web/screens/viewfile/Kronshtein_Geom.html");
+            viewFilesDc.getMutableItems().add(kronshtainGeom);
+
+            imageField.setVisible(false);
+            browseFrame.setVisible(false);
+        }
+
+        if (fileString == null || fileString.isEmpty()) {
+            dopFileView.setVisible(false);
+            dopFileView.setEnabled(false);
+        } else {
+            dopFileView.setVisible(true);
+            dopFileView.setEnabled(true);
+
+            if (fileString.contains(",")) {
+                String[] files = fileString.split(",");
+                for (String file : files) {
+                    file = file.substring(file.lastIndexOf("/") + 1);
+                    ViewFile vFile = metadata.create(ViewFile.class);
+                    vFile.setName(file);
+                    viewFilesDc2.getMutableItems().add(vFile);
+                }
+            } else {
+                ViewFile file = metadata.create(ViewFile.class);
+                file.setName(fileString);
+                viewFilesDc2.getMutableItems().add(file);
+            }
+        }
     }
 
     @Subscribe(id = "viewFilesDc", target = Target.DATA_CONTAINER)
     public void onViewFilesDcItemChange(InstanceContainer.ItemChangeEvent<ViewFile> event) {
         String path = viewFilesDc.getItem().getLink();
+        if (path == null)
+            return;
+
         if (path.contains("jpg")) {
             imageField.setVisible(true);
             imageField.setSource(ClasspathResource.class).setPath(path);
             browseFrame.setVisible(false);
-        } else {
+        } else if (path.contains("html")) {
             browseFrame.setVisible(true);
             browseFrame.setSource(ClasspathResource.class).setPath(path);
+            imageField.setVisible(false);
+        } else {
+            browseFrame.setVisible(false);
             imageField.setVisible(false);
         }
     }
